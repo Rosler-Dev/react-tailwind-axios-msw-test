2a4afe99bfd9f85eecd1c87894458a389222bcb1
Ryan <Rosler19@hotmail.com>
2025-07-16 01:33:45 -0700

    - convert JavaScript code into TypeScript.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
---
+++ src/components/Counter.tsx
@@ -0,0 +1,40 @@
+import React, { memo } from "react";
+
+const MAX_GUESTS: number = 99;
+
+function canDecrement(value: number): boolean {
+  return value > 0;
+}
+
+function canIncrement(value: number): boolean {
+  return value < MAX_GUESTS;
+}
+
+function decrement(value: number): number {
+  return canDecrement(value) ? value - 1 : value;
+}
+
+function increment(value: number): number {
+  return canIncrement(value) ? value + 1 : value;
+}
+
+interface CounterProps {
+  label: string;
+  value: number;
+  onChange: (value: number) => void;
+}
+
+const Counter = memo(function CounterMemo({label, value, onChange}: CounterProps) {
+  return (
+    <>
+      <p>{label}</p>
+      <button disabled={!canDecrement(value)} onClick={() => onChange(decrement(value))} >-</button>
+      <p className="text-center">{value}</p>
+      <button disabled={!canIncrement(value)} onClick={() => onChange(increment(value))} >+</button>
+    </>
+  );
+});
+
+Counter.displayName = 'Counter';
+
+export default Counter;
